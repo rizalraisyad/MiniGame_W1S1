5586e2e2dc4f798d502b4c2906dad1c0a2dd8aef
muharamrizal2 <muharamrizal2@gmail.com>
2021-07-29 17:54:05 +0700

    last commit

--- rspec_Modul4_sesi2/spec/increment_array_spec.rb
+++ rspec_Modul4_sesi2/spec/increment_array_spec.rb
@@ -8,4 +8,51 @@ RSpec.describe IncrementingArray do
 
     expect(result).to eq([2])
   end
+  it '[9] expected [1,0]' do
+    incrementingArray = IncrementingArray.new
+    
+    result = incrementingArray.increment([9])
+  
+    expect(result).to eq([1,0])
+  end
+
+  it '[1,0] expected [1,1]' do
+    incrementingArray = IncrementingArray.new
+    
+    result = incrementingArray.increment([1,0])
+  
+    expect(result).to eq([1,1])
+  end
+
+  it '[5,6,1] expected [5,6,2]' do
+    incrementingArray = IncrementingArray.new
+    
+    result = incrementingArray.increment([5,6,1])
+  
+    expect(result).to eq([5,6,2])
+  end
+
+  it '[1,0,0,0] expected [9,9,9]' do
+    incrementingArray = IncrementingArray.new
+    
+    result = incrementingArray.increment([9,9,9])
+  
+    expect(result).to eq([1,0,0,0])
+  end
+
+  it '[9,9] expected [1,0,0]' do
+    incrementingArray = IncrementingArray.new
+    
+    result = incrementingArray.increment([9,9])
+  
+    expect(result).to eq([1,0,0])
+  end
+
+  it '[3,9] expected [4,0]' do
+    incrementingArray = IncrementingArray.new
+    
+    result = incrementingArray.increment([3,9])
+  
+    expect(result).to eq([4,0])
+  end
 end
--- rspec_Modul4_sesi2/src/increment_array.rb
+++ rspec_Modul4_sesi2/src/increment_array.rb
@@ -1,6 +1,24 @@
 class IncrementingArray
   def increment(numbers)
-    numbers[numbers.length-1] += 1
+    index = numbers.length
+    index -= 1
+    temp_increment = 1
+    while index >= 0
+      numbers[index] += temp_increment
+      temp_increment -= 1 if temp_increment>0
+      if index - 1 >= 0
+        if numbers[index] == 10
+          numbers[index] = 0
+          temp_increment += 1
+        end
+      elsif index - 1 < 0
+        if numbers[index] == 10
+          numbers[index] = 0
+          numbers.unshift(1)
+        end
+      end
+      index -=1
+    end
     numbers
   end
 end
